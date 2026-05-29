9ca0635b0f943ffe2498360818ff9d593acf7845
Zheng Pei <zheng.pei.c@gmail.com>
2026-05-29 17:16:16 -0500

    fix typo of mrsf_roks import

--- lumeq/opt/__init__.py
+++ lumeq/opt/__init__.py
@@ -1,5 +1,5 @@
 from .optimization import gradient_descent, conjugate_gradient, newton_2nd
 from .riemannian import Riemannian, Grassmann
 
-from .mrsf_roks.py import MRSF_TDA
+from .mrsf_roks import MRSF_TDA
 from .electron_diffraction_uks import ElectronDiffractionUKS
